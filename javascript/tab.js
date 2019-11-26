f069b3bc94785414ead593b047893eea2bb0ba8d
lcijoyce622 <lcijoyce622@gmail.com>
2019-11-26 15:41:03 +0800

    footer修改

--- javascript/tab.js
+++ javascript/tab.js
@@ -1,18 +1,18 @@
-var btn_array=document.querySelectorAll('.tab_btn');
-    var article_array=document.querySelectorAll('.context_article');
-   
-    btn_array.forEach((btn,i) => {
-            btn.onclick= function(){
-            removeALL();
-            btn.classList.add('active');
-            article_array[i].classList.add('active');
-        }
-    });
-    function removeALL(){
-        btn_array.forEach(btn => {
-            btn.classList.remove('active');
-        });
-        article_array.forEach(article => {
-            article.classList.remove('active');
-        });
+var btn_array = document.querySelectorAll('.tab_btn');
+var article_array = document.querySelectorAll('.context_article');
+
+btn_array.forEach((btn, i) => {
+    btn.onclick = function () {
+        removeALL();
+        btn.classList.add('active');
+        article_array[i].classList.add('active');
     }
+});
+function removeALL() {
+    btn_array.forEach(btn => {
+        btn.classList.remove('active');
+    });
+    article_array.forEach(article => {
+        article.classList.remove('active');
+    });
+}
