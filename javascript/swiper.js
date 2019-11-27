b12f22c00bdbffd5cd9cbea7b27c1b6783fd1f5d
lcijoyce622 <lcijoyce622@gmail.com>
2019-11-27 23:08:53 +0800

    danger

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -1,33 +1,35 @@
 var swiper_news = new Swiper('#news_swiper', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        writable: true,
-        enumerable: true,
-        configurable: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-     
-      $(document).ready(function () {
-      $(window).resize(function(){
-        var ww = $(window).width()
-        if (ww>1200) {
-          swiper_news.params.slidesPerView = 3;}
-        if (ww<1200){ 
-          swiper_news.params.slidesPerView = 1;}
-        // swiper_news.reInit()
-      });
-      // $(window).trigger('resize')
-    });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  writable: true,
+  enumerable: true,
+  configurable: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+$(document).ready(function () {
+  $(window).resize(function () {
+    var ww = $(window).width()
+    if (ww > 1200) {
+      swiper_news.params.slidesPerView = 3;
+    }
+    if (ww < 1200) {
+      swiper_news.params.slidesPerView = 1;
+    }
+    // swiper_news.reInit()
+  });
+  // $(window).trigger('resize')
+});
 
 var swiper_protect = new Swiper('.protect_swiper', {
   //不能用id查找 事件裡的swiper-container太多 用另一個class抓取
@@ -40,4 +42,15 @@ var swiper_protect = new Swiper('.protect_swiper', {
   // //   nextEl: '.swiper-button-next',
   // //   prevEl: '.swiper-button-prev',
   // // },
+});
+
+var swiper = new Swiper('.danger_swiper', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
 });
