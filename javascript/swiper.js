3fb4a0d916dff965bf26ddace83446dabf183d5e
lcijoyce622 <lcijoyce622@gmail.com>
2019-11-27 12:12:46 +0800

    Update swiper.js

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -1,28 +1,4 @@
-// var swiper_news;
 var swiper_news = new Swiper('#news_swiper', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 1,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  writable: true,
-  enumerable: true,
-  configurable: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
-$(document).ready(function () {
-  $(window).resize(function () {
-    wdth = $(window).width();
-    if (wdth >= 1200) {
-      swiper_news = new Swiper('#news_swiper', {
         slidesPerView: 3,
         spaceBetween: 30,
         slidesPerGroup: 1,
@@ -40,32 +16,18 @@ $(document).ready(function () {
           prevEl: '.swiper-button-prev',
         },
       });
-
-    }
-    else {
-      swiper_news = new Swiper('#news_swiper', {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        writable: true,
-        enumerable: true,
-        configurable: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
+     
+      $(document).ready(function () {
+      $(window).resize(function(){
+        var ww = $(window).width()
+        if (ww>1200) {
+          swiper_news.params.slidesPerView = 3;}
+        if (ww<1200){ 
+          swiper_news.params.slidesPerView = 1;}
+        // swiper_news.reInit()
       });
-    }
-
-  });
-});
-
+      // $(window).trigger('resize')
+    });
 
 var swiper_protect = new Swiper('.protect_swiper', {
   //不能用id查找 事件裡的swiper-container太多 用另一個class抓取
