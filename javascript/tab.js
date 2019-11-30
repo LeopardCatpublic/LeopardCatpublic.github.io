faa43ec33255bb7149e1ace03cfd0d33d6576277
lcijoyce622 <lcijoyce622@gmail.com>
2019-11-30 22:54:14 +0800

    .

--- javascript/tab.js
+++ javascript/tab.js
@@ -1,11 +1,12 @@
 var btn_array = document.querySelectorAll('.tab_btn');
 var article_array = document.querySelectorAll('.context_article');
-
+var imgcontent=document.querySelectorAll('.img');
 btn_array.forEach((btn, i) => {
     btn.onclick = function () {
         removeALL();
         btn.classList.add('active');
         article_array[i].classList.add('active');
+        imgcontent[i].classList.add('active');
     }
 });
 function removeALL() {
@@ -14,5 +15,8 @@ function removeALL() {
     });
     article_array.forEach(article => {
         article.classList.remove('active');
+    });
+    imgcontent.forEach(img => {
+        img.classList.remove('active');
     });
 }
