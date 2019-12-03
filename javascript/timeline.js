bd4ec0274244806c973885207ed74c0d31c79fef
lcijoyce622 <lcijoyce622@gmail.com>
2019-12-03 22:50:43 +0800

    .

---
+++ javascript/timeline.js
@@ -0,0 +1,20 @@
+
+var timelineSwiper = new Swiper ('.timeline_swiper', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1000,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+ 
+  breakpoints: {
+   1200: {
+      direction: 'horizontal',
+    }
+  }
+});
